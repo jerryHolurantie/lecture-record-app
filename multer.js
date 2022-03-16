c1ede1ae92091b2e214737eb32db8c45759f06a1
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2022-03-16 11:16:39 +0100

    Update multer.js

--- multer.js
+++ multer.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/recordings");
+    cb(null, "/recordings");
   },
   filename: (req, file, cb) => {
     const ext = file.mimetype.split("/")[1];
